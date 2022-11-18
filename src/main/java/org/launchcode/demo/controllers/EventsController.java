8cbcea9738d0139fe20080d2fbad645a72d662ab
adriana darris <adrianadarris@gmail.com>
2022-11-18 15:13:37 -0600

    completed 13.6.4

--- src/main/java/org/launchcode/demo/controllers/EventsController.java
+++ src/main/java/org/launchcode/demo/controllers/EventsController.java
@@ -1,8 +1,10 @@
 package org.launchcode.demo.controllers;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,17 @@ import java.util.List;
 @RequestMapping("events")
 public class EventsController {
 
-    public String handlerMethod(Model model){
-        List events = new ArrayList();
+    @GetMapping
+    public String displayEvents(Model model){
+        List<String> events = new ArrayList();
         events.add("Code Till Dawn");
         events.add("Can't Stop Coding");
         events.add("Resume Support for new developers");
         model.addAttribute("events", events);
         return "events/index";
+    }
+    @GetMapping("create")
+    public String renderCreateEvent(){
+        return "events/create";
     }
 }
--- src/main/java/org/launchcode/demo/controllers/HomeController.java
+++ src/main/java/org/launchcode/demo/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package org.launchcode.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@GetMapping("index")
 public class HomeController {
-
+    @GetMapping
+    public String index(){
+        return "index";
+    }
 
 }
